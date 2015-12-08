4b17a4994e32f6ff7becceb9591c132446e2f3a3
tezuka24 <gbftony206@hotmail.com>
2015-12-08 16:37:23 +0100

    maj class

--- TEST_GITHUB/src/com/adaming/bo/Client.java
+++ TEST_GITHUB/src/com/adaming/bo/Client.java
@@ -1,5 +1,6 @@
 package com.adaming.bo;
 
 public class Client {
-
+	private int id;
+	private String nom;
 }
--- TEST_GITHUB/src/com/adaming/bo/Produit.java
+++ TEST_GITHUB/src/com/adaming/bo/Produit.java
@@ -2,4 +2,6 @@ package com.adaming.bo;
 
 public class Produit {
 
+	private int id;
+	private String nom;
 }
